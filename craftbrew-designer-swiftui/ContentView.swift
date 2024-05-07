6154c42d148fabf4751987e9c3a7096926734b20
davevanfleet <vanfleet@arsome.com>
2024-05-07 14:59:45 -0400

    Initial Commit

---
+++ craftbrew-designer-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  craftbrew-designer-swiftui
+//
+//  Created by David Van Fleet on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ craftbrew-designer-swiftui/craftbrew_designer_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  craftbrew_designer_swiftuiApp.swift
+//  craftbrew-designer-swiftui
+//
+//  Created by David Van Fleet on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct craftbrew_designer_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
